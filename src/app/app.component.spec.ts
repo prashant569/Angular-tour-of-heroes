4d832de7c92863c0c9522670669cd268e811393e
prashant shukla <ps04007@gmail.com>
2018-12-13 13:03:28 +0530

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'angular-tour-of-heroes'`, async(() => {
+  it(`should have as title 'Tour of Heroes'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-tour-of-heroes');
+    expect(app.title).toEqual('Tour of Heroes');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-tour-of-heroes!');
+    expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
   }));
 });
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from  './hero';
+
+export const HEROES : Hero [] = [
+
+{ id : 11 ,name: 'Mr Nice'},
+{ id : 12 ,name: 'Mr james'},
+{ id : 13 ,name: 'Mr Prashant'},
+{ id : 14 ,name: 'Mr Aadhar'},
+{ id : 15 ,name: 'Mr Caprio'},
+{ id : 16 ,name: 'Mr Bond'},
+{ id : 17 ,name: 'Mr Brook'},
+{ id : 18 ,name: 'Mr Jenny'},
+{ id : 19 ,name: 'Mr Hulk'},
+{ id : 20 ,name: 'Mr Groot'}
+
+];
